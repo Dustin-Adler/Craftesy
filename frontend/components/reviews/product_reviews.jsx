8719ebfba8b086a0397ba70e7f388d10083e85fa
dustin <dustin.adler@yahoo.com>
2021-10-20 01:27:55 -0500

    item show reviews rendering from show page

--- frontend/components/reviews/product_reviews.jsx
+++ frontend/components/reviews/product_reviews.jsx
@@ -26,57 +26,28 @@ class ProductReviews extends React.Component {
         }
     }
 
-    render(){
-        
-        // const rating = [...Array(5)].map( (el, i) => {
-        //     if(this.props.review < i+1){
-        //         return (< >
-                    
-        //             <input 
-        //                 className='review-star' 
-        //                 type="radio" 
-        //                 name='rating' 
-        //                 value={i+1} 
-        //                 id = {`rating-${i+1}`}/>
-        //             <label htmlFor={`rating-${i+1}`}>
-        //                 <i className="fas fa-star"></i>
-        //             </label>
-        //         </>)
-        //     } else {
-        //         return (<>
-        //             <input 
-        //                 className='review-star' 
-        //                 type="radio" name='rating' 
-        //                 value={i+1} 
-        //                 id={`rating-${i+1}`}/>
-        //             <label htmlFor={`rating-${i+1}`}>
-        //                 <i className="far fa-star"></i>
-        //             </label>
-        //         </>)
-        //     }
-        // })
-        
-
-        // const review = [...Array(5)].map( (el, i) => {
-        //     if(this.state.rating >= i+1){
-        //         return (
-        //         <li key={i}>
-        //             <input className='review-star' type="radio" name='rating' value={i+1} id={`rating-${i+1}`}/>
-        //             <label htmlFor={`rating-${i+1}`}>
-        //                 <i aria-hidden="true" className="fas fa-star"></i>
-        //             </label>
-        //         </li>)
-        //     } else {
-        //         return (
-        //         <li key={i}>
-        //             <input className='review-star' type="radio" name='rating' value={i+1} id={`rating-${i+1}`}/>
-        //             <label htmlFor={`rating-${i+1}`}>
-        //                 <i aria-hidden="true" className="far fa-star"></i>
-        //             </label>
-        //         </li>)
-        //     }
+    rating(rating) {
+        const stars = [...Array(5)].map( (el, i) => {
+            if(rating >= i+1){
+                return (
+                // <li key={i}>
+                    <i aria-hidden="true" className="fas fa-star"></i>
+                // </li>
+                )
+            } else {
+                return (
+                // <li key={i}>
+                    <i aria-hidden="true" className="far fa-star"></i>
+                // </li>
+                )
+            }
             
-        // })
+        })
+
+        return stars
+    }
+
+    render(){
 
         const productReviews = this.props.reviews.map(
             (review, i) =>
@@ -87,7 +58,7 @@ class ProductReviews extends React.Component {
                         {/* <div>{review.author_id}</div> */}
                         {/* <div>placeholder for timestamp</div> */}
                     </div>
-                    {/* {rating} */}
+                    {this.rating(review.rating)}
                     <p>{review.body}</p>
                     {/* <img className='review-image' src="" alt="Reviewer's Photo" /> */}
                     {/* <div>placeholder for helpful button</div> */}
