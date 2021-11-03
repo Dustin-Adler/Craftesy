8cb1085d3e87b717b7c11fefb17647cb76776f81
dustin <dustin.adler@yahoo.com>
2021-11-03 17:30:16 -0500

    cart-item component added and working

---
+++ db/migrate/20211103164724_add_default_value_to_carts.rb
@@ -0,0 +1,7 @@
+class AddDefaultValueToCarts < ActiveRecord::Migration[5.2]
+  def change
+    change_column_default :carts, :quantity, to: 1
+    change_column_null :carts, :shopper_id, false
+    change_column_null :carts, :product_id, false
+  end
+end
--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
-    faCaretDown,
     faHandshake, 
     faShippingFast, 
-    faStore, 
     faTag 
 } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import CartItem from './cart-item'
 
 class Cart extends React.Component {
     constructor(props){
@@ -24,107 +23,13 @@ class Cart extends React.Component {
         if (!this.props.cart) {
             return (null)
         } 
-        const currentDate = new Date().toDateString()
         const cartItems = this.props.cart.map(
             (cartItem, i) => {
                 // debugger
                 return (
-                    <div 
-                        className='cart-item-column-container item-seperator'
-                        key={i}>
-                        <div className='cart-item-row-container'>
-                            <div className='cart-item-row-container center' >
-                                <div className='shop-icon'><FontAwesomeIcon icon={faStore}/></div>
-                                <h3>Thank you for using Craftesy</h3>
-                            </div>
-                            <div className='center'>Link peronal page here</div>
-                        </div>
-                        <div className='cart-item-row-container'>
-                            <div className='cart-item-row-container cart-item-info'>
-                                <Link
-                                    className='link' 
-                                    to={`/products/${cartItem.id}`}>
-                                    <img 
-                                        className='cart-item-img' 
-                                        src={cartItem.images[0].url} 
-                                        alt={cartItem.name}/>
-                                </Link>
-                                <div className='cart-item-column-container padding-left'>
-                                    <div className='cart-item-column-container '>
-                                        <Link
-                                            className='text-link'
-                                            to={`/products/${cartItem.id}`}>
-                                            <div>{cartItem.name}</div>
-                                        </Link>
-                                        <div className='cart-item-options'></div>
-                                    </div>
-                                    <div className='sr-buttons'>
-                                        <button
-                                            className='plainify-button'>
-                                            Remove
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='cart-item-column-container column-right'>
-                                <div className='cart-item-row-container width cart-item-info '>
-                                    <div className='relative'>
-                                        <select className='item-quantity'>
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                            <option value="6">6</option>
-                                            <option value="7">7</option>
-                                            <option value="8">8</option>
-                                            <option value="9">9</option>
-                                            <option value="10">10</option>
-                                        </select>
-                                        <FontAwesomeIcon 
-                                            icon={faCaretDown} 
-                                            className='fa-caret-down'/>
-                                    </div>
-                                    <div>
-                                        <h3>$ {cartItem.price.toFixed(2)}</h3>
-                                        <p>($ {cartItem.price.toFixed(2)} each)</p>
-                                    </div>
-                                </div>
-                                <div className='popularity'>
-                                    XX people have this in their cart
-                                </div>
-                            </div>
-                        </div>
-                        <div className='cart-item-row-container'>
-                            <div className='cart-item-row-container start'>
-                                <input
-                                    className='checkbox' 
-                                    type="checkbox" />
-                                <div className='cart-item-column-container'>
-                                    <h6>Would you like this as a gift?</h6>
-                                    <p className='greyed-out'>
-                                        Prices aren't real this is a demo
-                                    </p>
-                                </div>
-                            </div>
-                            <button className='plainify-button center'>
-                                <FontAwesomeIcon icon={faTag} className='coupon-tag center'/>
-                                Apply shop coupon codes
-                            </button>
-                        </div>
-                        <div className='cart-item-row-container'>
-                            <textarea 
-                                placeholder="Add a note to Craftesy (optional)">
-                            </textarea>
-                            <div className='cart-item-column-container column-right delivery-info'>
-                                <p>
-                                    Estimated delivery: 
-                                    <span>{currentDate}</span>
-                                </p>
-                                <p>from: The Interweb</p>
-                            </div>
-                        </div>
-                    </div>
+                    <CartItem 
+                        key={i}
+                        cartItem={cartItem}/>
                 )
             }
         )
