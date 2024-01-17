3267e6abfcb0bd102182d509f497f0ecb6f003d9
dustin <dustin.adler@yahoo.com>
2024-01-17 16:35:06 -0600

    upgrades version of rails, ruby, and dependencies

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module Craftesy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    # config.load_defaults 5.2 - app originally written using rails 5.2 updating to 7
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
