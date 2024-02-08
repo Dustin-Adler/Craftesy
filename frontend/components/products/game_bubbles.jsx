a417727ea56b71d15b6e1d5d8339a69c94534d4f
dustin <dustin.adler@yahoo.com>
2024-02-08 11:55:55 -0600

    updates logic for game bubbles component

--- frontend/components/products/game_bubbles.jsx
+++ frontend/components/products/game_bubbles.jsx
@@ -5,45 +5,48 @@ class GameBubbles extends React.Component {
         super(props)
     }
 
+    componentDidMount(){
+        const numberOfImages = Object.keys(this.props.images).length;
+        if (numberOfImages < 6) {
+            this.props.getProducts()
+        }
+    }
+
     render() {
-      
-        // if (this.props.products === {}){
-        //     return null
-        // } else { 
-            return (
-                <div className='game-bubbles'>
-                    <div className='background-style'>
-                        <h2>Check out our unique in-game items</h2>
+        const images = this.props.images;
+        return (
+            <div className='game-bubbles'>
+                <div className='background-style'>
+                    <h2>Check out our unique in-game items</h2>
+                </div>
+                <div className='game-art-bubbles'>
+                    <div>
+                        <img className='circle-splash' src={images.mario} alt="Super Mario" />
+                        <h5>Super Mario</h5>
                     </div>
-                    <div className='game-art-bubbles'>
-                        <div>
-                            <img className='circle-splash' src={this.props.images.mario} alt="Super Mario" />
-                            <h5>Super Mario</h5>
-                        </div>
-                        <div>
-                            <img className='circle-splash sword' src={this.props.images.ffvii} alt="Buster Sword" />
-                            <h5>Final Fantasy</h5>
-                        </div>
-                        <div>
-                            <img className='circle-splash' src={this.props.images.sonic} alt="Super Speed Boots" />
-                            <h5>Sonic</h5>
-                        </div>
-                        <div>
-                            <img className='circle-splash' src={this.props.images.zelda} alt="Heart Container" />
-                            <h5>Zelda</h5>
-                        </div>
-                        <div>
-                            <img className='circle-splash llama' src={this.props.images.fortnite} alt="Llama Pinata" />
-                            <h5>Fortnite</h5>
-                        </div>
-                        <div>
-                            <img className='circle-splash' src={this.props.images.lol} alt="Trinity Force" />
-                            <h5>League</h5>
-                        </div>
+                    <div>
+                        <img className='circle-splash sword' src={images.ffvii} alt="Buster Sword" />
+                        <h5>Final Fantasy</h5>
+                    </div>
+                    <div>
+                        <img className='circle-splash' src={images.sonic} alt="Super Speed Boots" />
+                        <h5>Sonic</h5>
+                    </div>
+                    <div>
+                        <img className='circle-splash' src={images.zelda} alt="Heart Container" />
+                        <h5>Zelda</h5>
+                    </div>
+                    <div>
+                        <img className='circle-splash llama' src={images.fortnite} alt="Llama Pinata" />
+                        <h5>Fortnite</h5>
+                    </div>
+                    <div>
+                        <img className='circle-splash' src={images.lol} alt="Trinity Force" />
+                        <h5>League of Legends</h5>
                     </div>
                 </div>
-            )
-        // }
+            </div>
+        )
     }
 }
 
--- frontend/components/products/game_bubbles_container.js
+++ frontend/components/products/game_bubbles_container.js
@@ -6,33 +6,32 @@ const mSTP = (state) => {
     const products = Object.values(state.entities.products)
     const imgs = {}
     for (let i = 0; i < products.length; i++) {
-        const element = products[i];
-        switch (element.name) {
+        const product = products[i];
+        switch (product.name) {
             case "Cape Feather":
-                imgs['mario'] = element.images[1].url
+                imgs['mario'] = product.images[1].url
                 break;
             case "Cloud's Buster Sword":
-                imgs['ffvii'] = element.images[0].url
+                imgs['ffvii'] = product.images[0].url
                 break;
             case "Speed Boots":
-                imgs['sonic'] = element.images[0].url
+                imgs['sonic'] = product.images[0].url
                 break;
             case "Heart Container":
-                imgs['zelda'] = element.images[0].url
+                imgs['zelda'] = product.images[0].url
                 break;
             case "Llama Pinata":
-                imgs['fortnite'] = element.images[0].url
+                imgs['fortnite'] = product.images[0].url
                 break;
             case "Trinity Force":
-                imgs['lol'] = element.images[0].url
+                imgs['lol'] = product.images[0].url
                 break;
             default:
                 break;
         }
     }
     return {
-        images: imgs,
-        products: state.entities.products
+        images: imgs
     }
 }
 
