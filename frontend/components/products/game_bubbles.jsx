428b92b35e52498a928c8f5d96a2ac3898ab1b65
dustin <dustin.adler@yahoo.com>
2024-02-09 09:11:15 -0600

    adds hover transitions for game_bubble links

--- frontend/components/products/game_bubbles.jsx
+++ frontend/components/products/game_bubbles.jsx
@@ -35,32 +35,38 @@ class GameBubbles extends React.Component {
                 <div className='background-style'>
                     <h2>Check out our unique in-game items</h2>
                 </div>
-                <div className='game-art-bubbles'>
-                    <div onClick={() => this.handleClickBubble('mario')}>
-                        <img className='circle-splash' src={images.mario} alt="Super Mario" />
-                        <h5>Super Mario</h5>
-                    </div>
-                    <div onClick={() => this.handleClickBubble('final fantasy')}>
-                        <img className='circle-splash sword' src={images.ffvii} alt="Buster Sword" />
-                        <h5>Final Fantasy</h5>
-                    </div>
-                    <div onClick={() => this.handleClickBubble('sonic')}>
-                        <img className='circle-splash' src={images.sonic} alt="Super Speed Boots" />
-                        <h5>Sonic</h5>
-                    </div>
-                    <div onClick={() => this.handleClickBubble('zelda')}>
-                        <img className='circle-splash' src={images.zelda} alt="Heart Container" />
-                        <h5>Zelda</h5>
-                    </div>
-                    <div onClick={() => this.handleClickBubble('fortnite')}>
-                        <img className='circle-splash llama' src={images.fortnite} alt="Llama Pinata" />
-                        <h5>Fortnite</h5>
-                    </div>
-                    <div onClick={() => this.handleClickBubble('league of legends')}>
-                        <img className='circle-splash' src={images.lol} alt="Trinity Force" />
-                        <h5>League of Legends</h5>
-                    </div>
-                </div>
+                <ul className='game-art-bubbles'>
+                    <li onClick={() => this.handleClickBubble('mario')}
+                        className='circle-splash-container'>
+                            <img className='circle-splash' src={images.mario} alt="Super Mario"/>
+                            <h5>Super Mario</h5>
+                    </li>
+                    <li onClick={() => this.handleClickBubble('final fantasy')}
+                        className='circle-splash-container'>
+                            <img className='circle-splash sword' src={images.ffvii} alt="Buster Sword" />
+                            <h5>Final Fantasy</h5>
+                    </li>
+                    <li onClick={() => this.handleClickBubble('sonic')}
+                        className='circle-splash-container'>
+                            <img className='circle-splash' src={images.sonic} alt="Super Speed Boots" />
+                            <h5>Sonic</h5>
+                    </li>
+                    <li onClick={() => this.handleClickBubble('zelda')}
+                        className='circle-splash-container'>
+                            <img className='circle-splash' src={images.zelda} alt="Heart Container" />
+                            <h5>Zelda</h5>
+                    </li>
+                    <li onClick={() => this.handleClickBubble('fortnite')}
+                        className='circle-splash-container'>
+                            <img className='circle-splash llama' src={images.fortnite} alt="Llama Pinata" />
+                            <h5>Fortnite</h5>
+                    </li>
+                    <li onClick={() => this.handleClickBubble('league of legends')}
+                        className='circle-splash-container'>
+                            <img className='circle-splash' src={images.lol} alt="Trinity Force" />
+                            <h5>League of Legends</h5>
+                    </li>
+                </ul>
             </div>
         )
     }
